9daacca42e2c44326cdebeb1d0d12289b4162b45
Rana Ebrahim <ranaelbeshlay246@gmail.com>
2024-09-18 14:09:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
+            });
+            app.UseStaticFiles();
+            app.Run();
+        }
+    }
+}
