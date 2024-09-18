85e17167ccf130a258efb1f26fc254f4ffa4edee
Rana Ebrahim <ranaelbeshlay246@gmail.com>
2024-09-18 14:12:21 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() //actions
+        {
+            return View();
+    
+
+        }
+    
+        public IActionResult Skills() //actions
+        {
+            return View();
+        }
+        public IActionResult Projects() //actions
+        {
+            return View();
+        }
+        public IActionResult Contact() //actions
+        {
+            return View();
+        }
+    }
+}
